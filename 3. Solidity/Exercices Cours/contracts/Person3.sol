448051f557941c7737500fbaa411d9d8ddd2a5f1
Loïs <snowin71@outlook.fr>
2021-11-03 23:27:39 +0100

    Added Person2.sol - Array exercise

---
+++ 3. Solidity/Exercices Cours/contracts/Person3.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.9;
+
+/** @notice Exercice - Array
+    @author Loïs L. */
+contract Whitelist {
+    struct Person {
+        string name;
+        uint age;
+    }
+
+    Person[] persons;
+}
