2593d8edf2a2157d7c0b292975ea63c04d314068
Loïs <snowin71@outlook.fr>
2021-11-09 10:24:16 +0100

    replaced truffle-assertions asserts with OpenZeppelin test-helpers asserts

--- 4. Truffle & CI-CD/Exercices Cyril/GarticTests/test/garticTests.js
+++ 4. Truffle & CI-CD/Exercices Cyril/GarticTests/test/garticTests.js
@@ -1,4 +1,5 @@
 const Gartic = artifacts.require("GarticPhone");
+const { expectRevert, expectEvent } = require('@openzeppelin/test-helpers')
 const { expect } = require('chai');
 
 contract("Gartic", accounts => {
@@ -32,9 +33,9 @@ contract("Gartic", accounts => {
             assert.equal(await this.gartic.getLastWord.call(), _secondWord, "Wrong new word");
         })
         it("should always reverts with the right reason", async function() {
-            await truffleAssertions.reverts(this.gartic.putNewWord(words[2], {from: player2}), "address already played");
-            await truffleAssertions.reverts(this.gartic.putNewWord(words[1], {from: accounts[2]}), "word is the same than the previous word");
-            await truffleAssertions.reverts(this.gartic.putNewWord("", {from: accounts[2]}), "word can't be empty");
+            await expectRevert(this.gartic.putNewWord(words[2], {from: player2}), "address already played");
+            await expectRevert(this.gartic.putNewWord(words[1], {from: accounts[2]}), "word is the same than the previous word");
+            await expectRevert(this.gartic.putNewWord("", {from: accounts[2]}), "word can't be empty");
         })
     })
 
@@ -49,7 +50,7 @@ contract("Gartic", accounts => {
             assert.equal(word, "tenth");
         })
         it("should revert transaction with: Ownable: caller is not the owner", async function() {
-           await truffleAssertions.reverts(this.gartic.getAllWords.call({from: player2}), 'Ownable: caller is not the owner');
+           await expectRevert(this.gartic.getAllWords.call({from: player2}), 'Ownable: caller is not the owner');
         })
         it("should return all the previous words put", async function() {
             let words = await this.gartic.getAllWords.call({from: starterOwner});
@@ -76,17 +77,17 @@ contract("Gartic", accounts => {
             let expectedTimestamp = resultBlock.timestamp;
             let exptectedState = Gartic.gameStates.GUESS_WAITING;
 
-            truffleAssertions.eventEmitted(result, 'onStateChange', (ev) => {
+            expectEvent(result, 'onStateChange', (ev) => {
                 return ev.newState.toNumber() === exptectedState && ev.timestamp.toNumber() === expectedTimestamp;
             });
         })
         it("should try to put a word and revert", async function() {
-            await truffleAssertions.reverts(this.gartic.putNewWord("tooMuch", {from: accounts[20]}), "you can't enter word anymore");
+            await expectRevert(this.gartic.putNewWord("tooMuch", {from: accounts[20]}), "you can't enter word anymore");
         })
         it("should try a word and don't emit the end of game", async function() {
             let result = await this.gartic.guessFirstWord("wrong", {from: player6});
             //guessCount: 0 -> 1
-            truffleAssertions.eventNotEmitted(result, 'onFinishedGame');
+            expectEvent.notEmitted(result, 'onFinishedGame');
         })
         it("should try a word and emit the end of game and state change", async function() {
             let result = await this.gartic.guessFirstWord(_firstWord, {from: player6});
@@ -96,10 +97,10 @@ contract("Gartic", accounts => {
             let expectedTimestamp = resultBlock.timestamp;
             let expectedState = Gartic.gameStates.FINISHED;
 
-            truffleAssertions.eventEmitted(result, 'onFinishedGame', (ev) => {
+            expectEvent(result, 'onFinishedGame', (ev) => {
                 return ev.firstWord === _firstWord && ev.numberOfTry.toNumber() === 2;
             })
-            truffleAssertions.eventEmitted(result, 'onStateChange', (ev) => {
+            expectEvent(result, 'onStateChange', (ev) => {
                 return ev.newState.toNumber() === expectedState && ev.timestamp.toNumber() === expectedTimestamp;
             })
         })
