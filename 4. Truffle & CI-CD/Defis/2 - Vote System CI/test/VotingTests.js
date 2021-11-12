9aabfc961da61318e807cea5feea858d3a028901
Loïs <snowin71@outlook.fr>
2021-11-12 01:01:23 +0100

    added final tests

--- 4. Truffle & CI-CD/Defis/2 - Vote System CI/test/VotingTests.js
+++ 4. Truffle & CI-CD/Defis/2 - Vote System CI/test/VotingTests.js
@@ -266,6 +266,46 @@ contract(Voting, accounts => {
 
     /** 'Ending' context include all the tests about the talling of votes and checking step */
     context("Ending", () => {
-        //TODO
+        before(async () => {
+            /** Initial state: 
+         * - contract is deployed by the admin address
+         * - voter1, voter2 & voter3 are registered 
+         * - 1 proposal is submitted by each registered voter 
+         * - each voters voted: voter1/voter3 -> Proposal of voter3, voter3 -> Proposal of voter1 */
+            this.voting = await Voting.new({from: admin});
+
+            await this.voting.addVoter(voter1, {from: admin});
+            await this.voting.addVoter(voter2, {from: admin});
+            await this.voting.addVoter(voter3, {from: admin});
+
+            await this.voting.startProposalsRegistering({from: admin})
+
+            await this.voting.addProposal("Proposal of voter1", {from: voter1});
+            await this.voting.addProposal("Proposal of voter2", {from: voter2});
+            await this.voting.addProposal("Proposal of voter3", {from: voter3});
+
+            await this.voting.endProposalsRegistering({from: admin});
+            await this.voting.startVotingSession({from: admin});
+
+            await this.voting.setVote(2, {from: voter1});
+            await this.voting.setVote(2, {from: voter2});
+            await this.voting.setVote(0, {from: voter3});
+
+            await this.voting.endVotingSession({from: admin});
+        })
+        context("tally votes testing", () => {
+            it("should tally votes and return Proposal of voter3 as winner", async () => {
+                let receipt = await this.voting.tallyVotes({from: admin});
+                let winningProposalObject = await this.voting.getWinner();
+
+                expectEvent(
+                    receipt,
+                    'WorkflowStatusChange',
+                    {previousStatus: new BN(Voting.WorkflowStatus.VotingSessionEnded), newStatus: new BN(Voting.WorkflowStatus.VotesTallied)}
+                )
+                expect(winningProposalObject.description).to.be.equal("Proposal of voter3");
+                expect(winningProposalObject.voteCount).to.be.equal('2');
+            })
+        })
     })
 })
