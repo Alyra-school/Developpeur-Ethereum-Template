17f22ea56fb265e6703d2d69f123d46d0d9fb929
MarcOreliOoo <94016378+MarcOreliOoo@users.noreply.github.com>
2021-11-26 13:02:31 +0100

    Update Whitelist.sol

--- 3. Solidity/Learning/Whitelist.sol
+++ 3. Solidity/Learning/Whitelist.sol
@@ -1,4 +1,14 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+
 contract Whitelist {
 	mapping (address => bool) whiteList ;
+	struct Person{ //structure de données
+		string name;
+		uint age;
+	}
+
+	function addPersonn (string memory _name, uint _age) public pure {
+		Person memory unePersonne = Person(_name, _age);
+	}
 }
