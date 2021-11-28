96433ab4a3846b8c886f1c4ed339f231bb0efa5f
MarcOreliOoo <94016378+MarcOreliOoo@users.noreply.github.com>
2021-11-28 18:45:33 +0100

    Update ERC20Token.sol

--- 3. Solidity/Learning/ERC20Token.sol
+++ 3. Solidity/Learning/ERC20Token.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.10;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
+    uint public rate = 200;
+    uint public minInvest = 100000000000000000 wei; //0.1 etc = 1*10^18 / 10 = 10^17
+
     constructor() ERC20("ALYRA", "ALY") {
+
    }
 }
