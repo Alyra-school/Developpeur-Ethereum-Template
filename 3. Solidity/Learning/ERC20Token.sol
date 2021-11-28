1adf7c1e259379e1c531b4cb3c75aa6d84404e3e
MarcOreliOoo <94016378+MarcOreliOoo@users.noreply.github.com>
2021-11-28 19:50:28 +0100

    Update ERC20Token.sol

--- 3. Solidity/Learning/ERC20Token.sol
+++ 3. Solidity/Learning/ERC20Token.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
-    uint public rate = 200;
-    uint public minInvest = 100000000000000000 wei; //0.1 etc = 1*10^18 / 10 = 10^17
-
-    constructor() ERC20("ALYRA", "ALY") {
-
+    constructor(uint256 _initialSupply) ERC20("ALYRA", "ALY") {
+        _mint(msg.sender,_initialSupply);
    }
 }
