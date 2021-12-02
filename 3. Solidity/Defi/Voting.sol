a9e441fa5c393fa208f8a3ce8c30dd5bd3e91f17
MarcOreliOoo <94016378+MarcOreliOoo@users.noreply.github.com>
2021-12-02 18:38:14 +0100

    Update Voting.sol

--- 3. Solidity/Defi/Voting.sol
+++ 3. Solidity/Defi/Voting.sol
@@ -3,10 +3,19 @@ pragma solidity 0.8.10;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
+/*
+    @admin : déploye le contrat
+    1) L'administrateur du vote enregistre une liste blanche d'électeurs identifiés par leur adresse Ethereum.
+    2) L'administrateur du vote commence la session d'enregistrement de la proposition.
+    3) Les électeurs inscrits sont autorisés à enregistrer leurs propositions pendant que la session d'enregistrement est active.
+    4) L'administrateur de vote met fin à la session d'enregistrement des propositions.
+    5) L'administrateur du vote commence la session de vote.
+    6) Les électeurs inscrits votent pour leurs propositions préférées.
+    7) L'administrateur du vote met fin à la session de vote.
+    8) L'administrateur du vote comptabilise les votes.
+    9) Tout le monde peut vérifier les derniers détails de la proposition gagnante.
+*/
 contract Voting is Ownable{
-    event WhiteListed (address _address);
-    event BlackListed (address _address);
-
     event VoterRegistered(address voterAddress); 
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
@@ -21,31 +30,33 @@ contract Voting is Ownable{
         VotesTallied
     }
 
-    mapping (address => bool) private comptesWL;
-    mapping (address => bool) private comptesBL;
-
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
     }
+
     struct Proposal {
         string description;
         uint voteCount;
     }
 
+    event WhiteListed (address _address);
+    event BlackListed (address _address);
+
+    mapping (address => bool) private comptesWL;
+    mapping (address => bool) private comptesBL;
+
+    /*1) L'administrateur du vote enregistre une liste blanche d'électeurs identifiés par leur adresse Ethereum.*/
     function whitelist(address _address) public onlyOwner{
-        require(!comptesWL[_address],"deja WL");
-        require(!comptesBL[_address],"deja BL");
+        require(!comptesWL[_address],"Already in the whitelist my dear");
+        //Màj status enum
+
+        //Création d'un Voter via Structure
+        //Màj Whitelist address => struct
         comptesWL[_address] = true;
         emit WhiteListed(_address);
-    }
-
-    function blacklist(address _address) public onlyOwner{
-        require(!comptesBL[_address],"deja BL");
-        require(!comptesWL[_address],"deja WL");
-        comptesBL[_address] = false;
-        emit BlackListed(_address);
+        emit VoterRegistered(_address); //Event électeur enregistré
     }
 
     function isWhiteListed(address _address) public view returns(bool){
