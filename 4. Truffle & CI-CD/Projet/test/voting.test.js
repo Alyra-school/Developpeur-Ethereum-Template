d05330078398eb0f1c78367f94fe88b27f2926a0
VicLam <tanacetifolia@gmail.com>
2022-06-23 15:08:00 +0200

    unit test project

---
+++ 4. Truffle & CI-CD/Projet/test/voting.test.js
@@ -0,0 +1,62 @@
+const voting = artifacts.require("Voting");
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+
+contract("Voting", accounts => { 
+    let votingInstance;
+
+    const owner = accounts[0];
+    const voter1 = accounts[1];
+    const voter2 = accounts[2];
+    const voter3 = accounts[3];
+
+    describe.skip("Setter", () => {
+        beforeEach(async () => {
+            votingInstance = await voting.new({ from: owner });
+            await votingInstance.addVoter(voter1, { from: owner })
+        });
+    });
+
+    describe.skip("Getter", () => {
+        beforeEach(async () => {
+            votingInstance = await voting.new({ from: owner });
+            await votingInstance.addVoter(voter1, { from: owner })
+        });
+
+        it("should voter is whitelisted", async () => {
+            const storedData = await votingInstance.getVoter(voter1, { from: voter1 });
+            expect(storedData.isRegistered).to.be.true;
+        });
+
+        it("should voter is not whitelisted", async () => {
+            const storedData = await votingInstance.getVoter(voter2, { from: voter1 });
+            expect(storedData.isRegistered).to.be.false;
+        });
+
+        it("should return proposal from id", async() => {
+            await votingInstance.startProposalsRegistering({ from: owner });
+            await votingInstance.addProposal("Premiere proposition", { from: voter1 });
+            const storedData = await votingInstance.getOneProposal(0, { from: voter1 });
+            // console.log(storedData);
+            expect(storedData.description).to.equal("Premiere proposition");
+            expect(new BN(storedData.voteCount)).to.be.bignumber.equal(new BN(0));
+        });
+    });
+
+    describe("Events tests", () => {
+        // Un voter est ajouté à la liste et proposal started est activé
+        beforeEach(async () => {
+            votingInstance = await voting.new({ from: owner });
+            await votingInstance.addVoter(voter1, { from: owner });
+            await votingInstance.startProposalsRegistering({ from: owner });
+        });
+
+        it("should return id 0 for first proposal", async() => {
+            await expectEvent(
+                await votingInstance.addProposal("Premiere proposition", { from: voter1 }),
+                "ProposalRegistered",
+                { proposalId: new BN(0) }
+            );
+        });
+    })
+});
