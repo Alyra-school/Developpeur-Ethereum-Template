04a2791ba1607402b7f9ee53a84becd0268f2af2
Baadisse <57837833+Badisse@users.noreply.github.com>
2022-10-08 14:39:32 +0200

    Create ERC20Token.sol

---
+++ 3. Solidity/ERC20Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.14;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+
+    constructor() ERC20("BADISSE", "BAD") {}
+}
