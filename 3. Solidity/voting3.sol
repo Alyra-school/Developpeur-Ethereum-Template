41335c922b447cb28baefae3f7a64a3c50d64ccb
Baadisse <57837833+Badisse@users.noreply.github.com>
2022-10-14 15:56:38 +0200

    Add multiple winners and get proposal description

--- 3. Solidity/voting3.sol
+++ 3. Solidity/voting3.sol
@@ -12,6 +12,7 @@ contract Voting is Ownable {
         bool hasVoted;
         uint votedProposalId;
     }
+
     struct Proposal {
         string description;
         uint voteCount;
@@ -29,7 +30,12 @@ contract Voting is Ownable {
     mapping (address => Voter) whitelist;
     WorkflowStatus status = WorkflowStatus.RegisteringVoters;
     Proposal[] proposals;
-    uint winningProposalId;
+    uint[] winningProposalIds;          // handle multiple winners
+
+    event VoterRegistered(address voterAddress); 
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
 
     modifier checkStatus(WorkflowStatus _status) {
         require(status == _status, "Invalid workflow status");
@@ -37,14 +43,17 @@ contract Voting is Ownable {
     }
 
     modifier onlyWhitelisted() {
-        require(whitelist[msg.sender].isRegistered, "You are not in the whitelist");
+        require(whitelist[msg.sender].isRegistered, 
+            "You are not in the whitelist");
         _;
     }
 
-    event VoterRegistered(address voterAddress); 
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-    event ProposalRegistered(uint proposalId);
-    event Voted (address voter, uint proposalId);
+    modifier checkProposalIdExist(uint _proposalId) {
+        // Avoid voting for genesys proposal
+        require(_proposalId > 0, "The id must be strictly superior to 0");
+        require(_proposalId < proposals.length, string.concat("No proposal matching this id. The id must be strictly inferior to ", Strings.toString(proposals.length)));
+        _;
+    }
 
     function addVoter(address _addr) public onlyOwner checkStatus(WorkflowStatus.RegisteringVoters) {
         require(!whitelist[_addr].isRegistered, "Already whitelisted");
@@ -53,17 +62,25 @@ contract Voting is Ownable {
     }
 
     function updateStatus(WorkflowStatus _newStatus) public onlyOwner {
-        require(status == WorkflowStatus(uint(_newStatus)-1), "You can not switch to this status. You are not in the appropriate status");
+        require(status == WorkflowStatus(uint(_newStatus)-1), 
+            "You can not switch to this status. You are not in the appropriate status");
         if(_newStatus == WorkflowStatus.ProposalsRegistrationStarted){
+            // Avoid counting non voters whitelisted
             proposals.push(Proposal("Genesys", 0));
         } else if(_newStatus == WorkflowStatus.ProposalsRegistrationEnded){
             require(proposals.length > 1, "Minimum one proposal must be sent");
         } else if(_newStatus == WorkflowStatus.VotesTallied){
+            uint winningProposalId = 0;
             for(uint i=1; i<proposals.length; i++){
-            if(proposals[i].voteCount > winningProposalId) {
-                winningProposalId = i;
+                if(proposals[i].voteCount > winningProposalId) {
+                    winningProposalId = i;
+                }
             }
-        }
+            for(uint i=1; i<proposals.length; i++){
+                if(proposals[i].voteCount == winningProposalId) {
+                    winningProposalIds.push(i);
+                }
+            }
         } else if(_newStatus == WorkflowStatus.RegisteringVoters) {
             revert("You can not return to RegisteringVoters status");
         }
@@ -77,21 +94,29 @@ contract Voting is Ownable {
         emit ProposalRegistered(proposals.length - 1);
     }
 
-    function vote(uint _proposalId) public onlyWhitelisted checkStatus(WorkflowStatus.VotingSessionStarted) {
+    function vote(uint _proposalId) public onlyWhitelisted checkStatus(WorkflowStatus.VotingSessionStarted) checkProposalIdExist(_proposalId) {
         require(!whitelist[msg.sender].hasVoted, "You have already voted");
-        require(_proposalId > 0, "The id must be strictly superior to 0");
-        require(_proposalId < proposals.length, string.concat("No proposal matching this id. The id must be strictly inferior to ", Strings.toString(proposals.length)));
         proposals[_proposalId].voteCount ++;
         whitelist[msg.sender].hasVoted = true;
         whitelist[msg.sender].votedProposalId = _proposalId;
         emit Voted(msg.sender, _proposalId);
     }
 
-    function getWinner() public view checkStatus(WorkflowStatus.VotesTallied) returns(uint) {
-        return winningProposalId;
+    function getProposalDescription(uint _proposalId) public view onlyWhitelisted checkProposalIdExist(_proposalId) returns(string memory) {
+        require(uint(status) > 1, "Proposal registration must be ended");
+        return proposals[_proposalId].description;
     }
 
-    function getStatus() public view returns(string memory) {
+    function getVoterVote(address _voter) public view onlyWhitelisted returns(uint) {
+        return whitelist[_voter].votedProposalId;
+    }
+
+    function getWinner() public view checkStatus(WorkflowStatus.VotesTallied) onlyWhitelisted returns(uint[] memory) {
+        return winningProposalIds;
+    }
+
+    // Only for development purpose. Not needed in prod because of the event WorkflowStatusChange
+    function getStatus() public view onlyOwner onlyWhitelisted returns(string memory) {
         if(status == WorkflowStatus.RegisteringVoters){
             return "RegisteringVoters";
         } else if(status == WorkflowStatus.ProposalsRegistrationStarted){
