24a76dea2dde809f46ccc534f9410c4158b42363
cyril marois <cyrilmarois@gmail.com>
2022-10-14 18:24:54 +0200

    test

--- 3. Solidity/Voting.sol
+++ 3. Solidity/Voting.sol
@@ -5,6 +5,8 @@ pragma solidity >= 0.7.0 <0.9.0;
 import "@OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
+    string currentStatus;
+
     struct Voter {
         bool isRegisted;
         bool hasVoted;
@@ -16,8 +18,10 @@ contract Voting is Ownable {
         uint voteCount;
 
     }
-
+    Proposal[] proposals;
     mapping(address => Voter) registeredVoters;
+    mapping(Voter => Proposal) voterProposal;
+
 
     enum WorkflowStatus {
         RegisteringVoters,
@@ -33,16 +37,55 @@ contract Voting is Ownable {
     event ProposalRegistedEvent(uint proposalId);
     event VotedEvent(address voter, uint proposalId);
 
-    function registerVoters(address[] voterAddressed) onlyOwner public {
-        for(uint i; i < voterAddressed.length; i++) {
-            Voter memory tmpVoter;
+    function registerVoters(address[] votersAddress) onlyOwner public {
+        for(uint i; i < votersAddress.length; i++) {
+            tmpVoter = Voter();
             tmpVoter.isRegisted = true;
             tmpVoter.hasVoted = false;
-            registeredVoters[i] = tmpVoter;
+            registeredVoters[votersAddress[i]] = tmpVoter;
             emit VoterRegisteredEvent(i);
         }
     }
 
-    function votersProposals
+    function voterProposal(string memory description, address voterAddress) public checkVoter {
+        changeStatus(WorkflowStatus['RegisteringVoters'], WorkflowStatus['ProposalsRegistrationStarted']);
 
+        proposals[] = Proposal(description);
+    }
+
+    function getWinner() public view returns(Proposal) {
+        Proposal proposalWinner;
+        for(uint i; i < proposals.length; i++) {
+            if (!tmpWinner
+                || tmpWinner.voteCount < proposals[i].voteCount
+            ) {
+                proposalWinner = proposals[i];
+            }
+        }
+
+        return proposalWinner;
+    }
+
+    function changeStatus(WorkflowStatus _previousStatus, WorkflowStatus _newStatus) {
+        emit WorkflowStatusChangeEvent(_previousStatus, _newStatus);
+        currentStatus = _newStatus;
+    }
+
+    function submitVote(uint memory proposalId, address _address) public {
+        changeStatus(WorkflowStatus['ProposalsRegistrationEnded'], WorkflowStatus['VotingSessionStarted']);
+        registeredVoters;
+    }
+
+    modifier checkVoter(address _address) {
+        require(registeredVoters[voterAddress].isRegistered == true, "Forbidden, u must be registered to continue");
+        _;
+    }
+
+    modifier checkStatus(WorkflowStatus _workflowStatus) {
+
+    }
+
+    function getCurrentStatus() pure view public {
+        return
+    }
 }
